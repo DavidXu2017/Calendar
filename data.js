6a3353a5633ab520ad1355aca633c080665493f6
Guangyu Xu <guangyuxu@Guangyus-iMac.lan>
2018-01-10 16:32:53 -0500

    did basic javascript logic

---
+++ data.js
@@ -0,0 +1,50 @@
+(function(){
+  let datepicker = {};
+  datepicker.getMonthData = function(year, month) {
+    let ret = [];
+    if (!year || !month) {
+      let today = new Date();
+      year = today.getFullYear();
+      month = today.getMonth() + 1;
+    }
+
+    let firstDay = new Date(year, month - 1, 1);
+    let firstDayWeekDay = firstDay.getDay();
+    if (firstDayWeekDay === 0) firstDayWeekDay = 7;
+
+    let lastDayOfLastMonth = new Date(year, month -1, 0);
+    let lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+    
+    let perMonthDayCount = firstDayWeekDay - 1;
+
+    let lastDay = new Date(year, month, 0);
+    let lastDate = lastDay.getDate();
+
+    for(let i = 0; i < 7*6; i++) {
+      let date = i + 1 - perMonthDayCount;
+      let showDate = date;
+      let thisMonth = month;
+
+      //last month
+      if(date <= 0) {
+        thisMonth = month - 1;
+        showDate = lastDateOfLastMonth + date;
+      } else if(date > lastDate) {
+        //next month
+        thisMonth = month + 1;
+        showDate = showDate - lastDate;
+      }
+
+      if (thisMonth === 0) thisMonth = 12;
+      if (thisMonth === 13) thisMonth = 1;
+
+      ret.push({
+        month: thisMonth,
+        date: date,
+        showDate: showDate
+      })
+    }
+    return ret;
+  }
+  window.datepicker = datepicker;
+})();
